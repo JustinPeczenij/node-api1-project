f229765bebb02f41a9a2fe1b23666e6e198f44b2
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-03 19:32:44 -0400

    user is able to get a list of users and a specfiic user from db

--- api/server.js
+++ api/server.js
@@ -4,6 +4,31 @@ const User = require('./users/model');
 
 const server = express();
 
-server.use(express.json);
+server.use(express.json());
+//ROUTES - I think they're called routes
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            console.log(users)
+            res.json(users)
+        }) 
+        .catch (() => {
+            res.status(500).json({
+                message: 'users information could not be retrieved',
+            })
+        })
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if(!user){
+            res.status(404).json( {message: "the user with the specified ID doesn't exist"} )
+        } else res.json(user)
+    } catch {
+        res.status(500).json( {message: "the user information could not be retrieved"} )
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
